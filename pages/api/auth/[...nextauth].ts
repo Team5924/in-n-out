f2096ac6737d457754ee79a5b6a38fa78e58b214
Chris Voon <christopher.voon@gmail.com>
2023-06-06 15:45:05 -0700

    Add prisma, tailwind, nextauth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,29 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/app/client";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID
+        ? process.env.GOOGLE_CLIENT_ID
+        : "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        ? process.env.GOOGLE_CLIENT_SECRET
+        : "",
+    }),
+  ],
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma),
+  theme: {
+    logo: "http://localhost:3000/in-n-out.png",
+  },
+  session: {
+    strategy: "database",
+    maxAge: 86400,
+    updateAge: 3600,
+  },
+};
+
+export default NextAuth(authOptions);
