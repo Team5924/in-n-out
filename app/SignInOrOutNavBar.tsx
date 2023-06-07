a29bf31b0b17edf347676a50f0c47987520c92bd
Chris Voon <christopher.voon@gmail.com>
2023-06-06 20:54:15 -0700

    Add SignInOrOutNavBar component

---
+++ app/SignInOrOutNavBar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+import { redirect } from "next/navigation";
+
+export type NavBarItem = {
+  name: string;
+  href: string;
+};
+
+export default function SignInOrOutNavBar({
+  isSignInBar,
+  navBarItems,
+}: {
+  isSignInBar: boolean;
+  navBarItems?: NavBarItem[];
+}) {
+  return (
+    <section>
+      <nav
+        className={`flex h-14 content-center items-center justify-between bg-accent px-6`}
+      >
+        <div>
+          {navBarItems?.map((navBarItem, index) => (
+            <Link
+              key={index}
+              href={navBarItem.href}
+              className="mx-6 text-xl font-bold transition-colors hover:text-yellow-300"
+            >
+              {navBarItem.name}
+            </Link>
+          ))}
+        </div>
+        <div>
+          {isSignInBar ? (
+            <p
+              className="mx-6 text-xl font-bold transition-colors hover:text-yellow-300"
+              onClick={() => {
+                if (status === "authenticated") {
+                  redirect("/hours");
+                } else {
+                  void signIn("google");
+                }
+              }}
+            >
+              Sign In
+            </p>
+          ) : (
+            <p
+              className="mx-6 text-xl font-bold transition-colors hover:text-yellow-300"
+              onClick={() => signOut()}
+            >
+              Sign Out
+            </p>
+          )}
+        </div>
+      </nav>
+    </section>
+  );
+}
