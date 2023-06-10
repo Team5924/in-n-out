b8d614fe57211f94c1148a10eb8034a6f8388cdc
Chris Voon <christopher.voon@gmail.com>
2023-06-09 17:56:23 -0700

    Don't check for session for page access twice (remove in page)

--- app/(clock-app)/hours/page.tsx
+++ app/(clock-app)/hours/page.tsx
@@ -3,16 +3,15 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function Hours() {
   const session = await getServerSession(authOptions);
-  if (session) {
-    return (
-      <div className="flex flex-grow flex-col items-center justify-center">
-        <h2 className="pb-9 text-5xl font-bold">
-          Hello {session.user?.name?.split(" ")[0]},
-        </h2>
-        <h3 className="text-4xl font-bold">You have</h3>
-        <h1 className="py-3 text-8xl font-extrabold">69</h1>
-        <h3 className="text-4xl font-bold">Hours</h3>
-      </div>
-    );
-  }
+
+  return (
+    <div className="flex flex-grow flex-col items-center justify-center">
+      <h2 className="pb-9 text-5xl font-bold">
+        Hello {session?.user?.name?.split(" ")[0]},
+      </h2>
+      <h3 className="text-4xl font-bold">You have</h3>
+      <h1 className="py-3 text-8xl font-extrabold">69</h1>
+      <h3 className="text-4xl font-bold">Hours</h3>
+    </div>
+  );
 }
