43a3d47982b06d5a7135d263b72d7e0a3aa28c34
Chris Voon <christopher.voon@gmail.com>
2023-06-15 22:19:40 -0600

    Make signout button disappear on outside click

--- app/ProfilePictureSignOut.tsx
+++ app/ProfilePictureSignOut.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { MdExitToApp } from "react-icons/md";
 import { signOut } from "next-auth/react";
 import { IconContext } from "react-icons";
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function ProfilePictureSignOut({
   signOutProfileImageSrc,
@@ -13,15 +14,17 @@ export default function ProfilePictureSignOut({
   return (
     <>
       {isOpen && (
-        <div
-          className="absolute right-7 top-16 flex items-center justify-start rounded-lg bg-white px-1 py-2.5 hover:bg-gray-300"
-          onClick={() => signOut()}
-        >
-          <IconContext.Provider value={{ color: "black" }}>
-            <MdExitToApp className="mx-1"></MdExitToApp>
-          </IconContext.Provider>
-          <p className="mx-1 font-semibold text-black">Sign Out</p>
-        </div>
+        <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
+          <div
+            className="absolute right-7 top-16 flex items-center justify-start rounded-lg bg-white px-1 py-2.5 hover:bg-gray-300"
+            onClick={() => signOut()}
+          >
+            <IconContext.Provider value={{ color: "black" }}>
+              <MdExitToApp className="mx-1"></MdExitToApp>
+            </IconContext.Provider>
+            <p className="mx-1 font-semibold text-black">Sign Out</p>
+          </div>
+        </OutsideClickHandler>
       )}
       <Image
         className="rounded-full outline-4 outline-yellow-300 transition-all hover:outline"
