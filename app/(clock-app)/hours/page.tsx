b7c79f78b7a426b2999fbb463241231bfe585aa6
Chris Voon <christopher.voon@gmail.com>
2023-06-18 17:57:19 -0600

    Use margin instead of padding

--- app/(clock-app)/hours/page.tsx
+++ app/(clock-app)/hours/page.tsx
@@ -7,11 +7,11 @@ export default async function Hours() {
 
   return (
     <div className="flex flex-grow flex-col items-center justify-center">
-      <h2 className="pb-9 text-5xl font-bold">
+      <h2 className="mb-9 text-5xl font-bold">
         Hello {session?.user?.name?.split(" ")[0]},
       </h2>
       <h3 className="text-4xl font-bold">You have</h3>
-      <h1 className="py-3 text-8xl font-extrabold">
+      <h1 className="my-3 text-8xl font-extrabold">
         {await getHours(session?.user?.email ?? "")}
       </h1>
       <h3 className="text-4xl font-bold">hours</h3>
