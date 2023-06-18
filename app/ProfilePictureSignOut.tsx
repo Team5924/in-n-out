738dd54d77d9f17ee808ab85523ad356c2536387
Chris Voon <christopher.voon@gmail.com>
2023-06-18 16:36:20 -0600

    Make clicking on profile picture while sign out popup is open close the popup

--- app/ProfilePictureSignOut.tsx
+++ app/ProfilePictureSignOut.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { MdExitToApp } from "react-icons/md";
 import { signOut } from "next-auth/react";
 import { IconContext } from "react-icons";
@@ -10,11 +10,19 @@ export default function ProfilePictureSignOut({
 }: {
   signOutProfileImageSrc: string;
 }) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const lastOutsideClickRef = useRef(new Date(0));
+
   return (
     <>
-      {isOpen && (
-        <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
+      {popupOpen && (
+        <OutsideClickHandler
+          onOutsideClick={() => {
+            setPopupOpen(false);
+            lastOutsideClickRef.current = new Date();
+            console.log(lastOutsideClickRef.current);
+          }}
+        >
           <div
             className="absolute right-7 top-16 flex items-center justify-start rounded-lg bg-white px-1 py-2.5 hover:bg-gray-300"
             onClick={() => signOut()}
@@ -28,7 +36,12 @@ export default function ProfilePictureSignOut({
       )}
       <Image
         className="rounded-full outline-4 outline-yellow-300 transition-all hover:outline"
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => {
+          const timeSinceLastOutsideClick =
+            Date.now() - lastOutsideClickRef.current.getTime();
+          console.log(timeSinceLastOutsideClick);
+          if (timeSinceLastOutsideClick >= 20) setPopupOpen(true);
+        }}
         alt="Profile picture"
         src={signOutProfileImageSrc}
         width={44}
