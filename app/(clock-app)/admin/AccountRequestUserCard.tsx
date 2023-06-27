c0fb235b21c2fd0c50baef22b45cbbafe264cf6b
Chris Voon <christopher.voon@gmail.com>
2023-06-27 19:44:47 -0400

    Use tailwind class instead of context for admin user approve and reject buttons

--- app/(clock-app)/admin/AccountRequestUserCard.tsx
+++ app/(clock-app)/admin/AccountRequestUserCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { BsCheckLg, BsXLg } from "react-icons/bs";
-import { IconContext } from "react-icons";
 import { useTransition } from "react";
 import { approveUser, rejectUser } from "@/app/actions";
 
@@ -17,18 +16,14 @@ export default function AccountRequestUserCard({
     <div className="m-2 flex w-5/6 items-center justify-between rounded border-2 border-white bg-secondary p-2 sm:w-2/3 md:w-7/12 xl:w-1/2">
       <p className="font-semibold text-gray-800">{`${name} (${email})`}</p>
       <div className="flex items-center">
-        <IconContext.Provider value={{ color: "green", size: "2.4em" }}>
-          <BsCheckLg
-            className="m-1 fill-gray-800 transition-all hover:scale-125 hover:fill-green-700"
-            onClick={() => startTransition(() => approveUser(email))}
-          ></BsCheckLg>
-        </IconContext.Provider>
-        <IconContext.Provider value={{ size: "1.9em" }}>
-          <BsXLg
-            className="m-1 fill-gray-800 transition-all hover:scale-125 hover:fill-red-700"
-            onClick={() => startTransition(() => rejectUser(email))}
-          ></BsXLg>
-        </IconContext.Provider>
+        <BsCheckLg
+          className="m-1 h-[2.4em] w-[2.4em] fill-gray-800 transition-all hover:scale-125 hover:fill-green-700"
+          onClick={() => startTransition(() => approveUser(email))}
+        ></BsCheckLg>
+        <BsXLg
+          className="m-1 h-[1.9em] w-[1.9em] fill-gray-800 transition-all hover:scale-125 hover:fill-red-700"
+          onClick={() => startTransition(() => rejectUser(email))}
+        ></BsXLg>
       </div>
     </div>
   );
