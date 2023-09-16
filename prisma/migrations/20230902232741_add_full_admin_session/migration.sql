f2d17ffa6273bfab41b7b97a83a53810a32da044
Chris Voon <christopher.voon@gmail.com>
2023-09-16 13:23:17 -0700

    Add isTerminal to user table in database

---
+++ prisma/migrations/20230902232741_add_full_admin_session/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Session` ADD COLUMN `fullAdmin` BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20230902234059_add_terminal_accounts/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `isTerminal` BOOLEAN NOT NULL DEFAULT false;
